4c0e8f3358a361332536241c7696f0ea2e9a54fa
aticcmahbub <aticcmahbub@gmail.com>
2026-01-11 01:27:54 +0600

    dp4

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -3,17 +3,87 @@ import {AuthService} from './auth.service.js';
 import {catchAsync} from '../../utils/catchAsync.js';
 import {sendResponse} from '../../utils/sendResponse.js';
 import {setAuthCookie} from '../../utils/setAuthCookie.js';
+import {envVars} from '../../../config/index.js';
 
+// const loginUser = catchAsync(async (req: Request, res: Response) => {
+//     const result = await AuthService.loginUser(req.body);
+
+//     setAuthCookie(res, result);
+
+//     sendResponse(res, {
+//         success: true,
+//         statusCode: 200,
+//         message: 'User logged in successfully',
+//         data: result,
+//     });
+// });
 const loginUser = catchAsync(async (req: Request, res: Response) => {
-    const result = await AuthService.loginUser(req.body);
+    const accessTokenExpiresIn = envVars.JWT.JWT_ACCESS_EXPIRES as string;
+    const refreshTokenExpiresIn = envVars.JWT.JWT_REFRESH_EXPIRES as string;
 
-    setAuthCookie(res, result);
+    // convert accessTokenExpiresIn to milliseconds
+    let accessTokenMaxAge = 0;
+    const accessTokenUnit = accessTokenExpiresIn.slice(-1);
+    const accessTokenValue = parseInt(accessTokenExpiresIn.slice(0, -1));
+    if (accessTokenUnit === 'y') {
+        accessTokenMaxAge = accessTokenValue * 365 * 24 * 60 * 60 * 1000;
+    } else if (accessTokenUnit === 'M') {
+        accessTokenMaxAge = accessTokenValue * 30 * 24 * 60 * 60 * 1000;
+    } else if (accessTokenUnit === 'w') {
+        accessTokenMaxAge = accessTokenValue * 7 * 24 * 60 * 60 * 1000;
+    } else if (accessTokenUnit === 'd') {
+        accessTokenMaxAge = accessTokenValue * 24 * 60 * 60 * 1000;
+    } else if (accessTokenUnit === 'h') {
+        accessTokenMaxAge = accessTokenValue * 60 * 60 * 1000;
+    } else if (accessTokenUnit === 'm') {
+        accessTokenMaxAge = accessTokenValue * 60 * 1000;
+    } else if (accessTokenUnit === 's') {
+        accessTokenMaxAge = accessTokenValue * 1000;
+    } else {
+        accessTokenMaxAge = 1000 * 60 * 60; // default 1 hour
+    }
+
+    // convert refreshTokenExpiresIn to milliseconds
+    let refreshTokenMaxAge = 0;
+    const refreshTokenUnit = refreshTokenExpiresIn.slice(-1);
+    const refreshTokenValue = parseInt(refreshTokenExpiresIn.slice(0, -1));
+    if (refreshTokenUnit === 'y') {
+        refreshTokenMaxAge = refreshTokenValue * 365 * 24 * 60 * 60 * 1000;
+    } else if (refreshTokenUnit === 'M') {
+        refreshTokenMaxAge = refreshTokenValue * 30 * 24 * 60 * 60 * 1000;
+    } else if (refreshTokenUnit === 'w') {
+        refreshTokenMaxAge = refreshTokenValue * 7 * 24 * 60 * 60 * 1000;
+    } else if (refreshTokenUnit === 'd') {
+        refreshTokenMaxAge = refreshTokenValue * 24 * 60 * 60 * 1000;
+    } else if (refreshTokenUnit === 'h') {
+        refreshTokenMaxAge = refreshTokenValue * 60 * 60 * 1000;
+    } else if (refreshTokenUnit === 'm') {
+        refreshTokenMaxAge = refreshTokenValue * 60 * 1000;
+    } else if (refreshTokenUnit === 's') {
+        refreshTokenMaxAge = refreshTokenValue * 1000;
+    } else {
+        refreshTokenMaxAge = 1000 * 60 * 60 * 24 * 30; // default 30 days
+    }
+    const result = await AuthService.loginUser(req.body);
+    const {refreshToken, accessToken} = result;
+    res.cookie('accessToken', accessToken, {
+        secure: true,
+        httpOnly: true,
+        sameSite: 'none',
+        maxAge: accessTokenMaxAge,
+    });
+    res.cookie('refreshToken', refreshToken, {
+        secure: true,
+        httpOnly: true,
+        sameSite: 'none',
+        maxAge: refreshTokenMaxAge,
+    });
 
     sendResponse(res, {
-        success: true,
         statusCode: 200,
-        message: 'User logged in successfully',
-        data: result,
+        success: true,
+        message: 'Logged in successfully!',
+        data: {refreshToken, accessToken},
     });
 });
 
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -2,39 +2,77 @@ import bcrypt from 'bcryptjs';
 import AppError from '../../errorHelpers/appError.js';
 import {createUserTokens} from '../../utils/userTokens.js';
 import {prisma} from '../../../lib/prisma.js';
-import {verifyToken} from '../../utils/jwt.js';
+import {generateToken, verifyToken} from '../../utils/jwt.js';
 import {envVars} from '../../../config/index.js';
-import type {JwtPayload} from 'jsonwebtoken';
+import type {JwtPayload, Secret} from 'jsonwebtoken';
+
+// const loginUser = async (payload: {email: string; password: string}) => {
+//     const user = await prisma.user.findUniqueOrThrow({
+//         where: {email: payload.email},
+//     });
+
+//     const isCorrectPassword = await bcrypt.compare(
+//         payload.password,
+//         user.password,
+//     );
+
+//     if (!isCorrectPassword) {
+//         throw new AppError(404, 'Password is not correct');
+//     }
+
+//     if (user.isDeleted) {
+//         throw new AppError(410, 'User is deleted');
+//     }
+
+//     const userTokens = createUserTokens(user);
+
+//     const userWithoutPassword = {...user, password: undefined};
+
+//     return {
+//         accessToken: userTokens.accessToken,
+//         refreshToken: userTokens.refreshToken,
+//         user: userWithoutPassword,
+//     };
+// };
 
 const loginUser = async (payload: {email: string; password: string}) => {
-    const user = await prisma.user.findUniqueOrThrow({
-        where: {email: payload.email},
+    const userData = await prisma.user.findUniqueOrThrow({
+        where: {
+            email: payload.email,
+        },
     });
 
-    const isCorrectPassword = await bcrypt.compare(
+    const isCorrectPassword: boolean = await bcrypt.compare(
         payload.password,
-        user.password,
+        userData.password,
     );
 
     if (!isCorrectPassword) {
-        throw new AppError(404, 'Password is not correct');
+        throw new Error('Password incorrect!');
     }
+    const accessToken = generateToken(
+        {
+            email: userData.email,
+            role: userData.role,
+        },
+        envVars.JWT.JWT_ACCESS_SECRET as Secret,
+        envVars.JWT.JWT_ACCESS_EXPIRES as string,
+    );
 
-    if (user.isDeleted) {
-        throw new AppError(410, 'User is deleted');
-    }
-
-    const userTokens = createUserTokens(user);
-
-    const userWithoutPassword = {...user, password: undefined};
+    const refreshToken = generateToken(
+        {
+            email: userData.email,
+            role: userData.role,
+        },
+        envVars.JWT.JWT_REFRESH_SECRET as Secret,
+        envVars.JWT.JWT_REFRESH_EXPIRES as string,
+    );
 
     return {
-        accessToken: userTokens.accessToken,
-        refreshToken: userTokens.refreshToken,
-        user: userWithoutPassword,
+        accessToken,
+        refreshToken,
     };
 };
-
 const refreshToken = async (token: string) => {
     let decodedData;
     try {
